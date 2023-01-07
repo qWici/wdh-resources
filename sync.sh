9f7432cf3922aec6f83b9672e6896ac6bd4afbc7
Ivan Kucher <dev.kucher@gmail.com>
2023-01-07 19:32:52 +0200

    test with API mock

--- sync.sh
+++ sync.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 case "$1" in
-"authors") echo "update authors";;
-"channels") echo "update channels";;
-"podcasts") echo "update podcasts";;
-"streamers") echo "update streamers";;
+"authors")
+  echo "Update authors"
+  REQ=$(curl -X POST -H "Content-Type: application/json" -d @./authors.json ${API_HOST}/author/sync-resource)
+  echo "Authors stored: $REQ"
+  ;;
+"channels")
+  echo "Update channels"
+  REQ=$(curl -X POST -H "Content-Type: application/json" -d @./channels.json ${API_HOST}/channel/sync-resource)
+  echo "Channels stored: $REQ"
+  ;;
+"podcasts")
+  echo "Update podcasts"
+  REQ=$(curl -X POST -H "Content-Type: application/json" -d @./podcasts.json ${API_HOST}/show/sync-resource)
+  echo "Podcasts stored: $REQ"
+  ;;
+"streamers")
+  echo "Update streamers"
+  REQ=$(curl -X POST -H "Content-Type: application/json" -d @./streamers.json ${API_HOST}/streamer/sync-resource)
+  echo "Streamers stored: $REQ"
+  ;;
 esac
-
-REQ=$(curl -X POST -H "Content-Type: application/json" -d @./authors.json http://localhost:3000/v1/author/sync-resource)
-#
-#echo "Authors stored $REQ"
