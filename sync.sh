3e1afb30f73a1e05d7fee72f58500b94e53eeae3
Ivan Kucher <dev.kucher@gmail.com>
2023-01-07 18:37:26 +0200

    start make github action

---
+++ sync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case "$1" in
+"authors") echo "update authors";;
+"channels") echo "update channels";;
+"podcasts") echo "update podcasts";;
+"streamers") echo "update streamers";;
+esac
+
+#REQ=$(curl -X POST -H "Content-Type: application/json" -d @./authors.json http://localhost:3000/v1/author/sync-resource)
+#
+#echo "Authors stored $REQ"
